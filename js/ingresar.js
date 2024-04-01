65d98d5e10a66bd7123da6fbf87c22b6a2f2da35
MaicolRuiz01 <106454008+MaicolRuiz01@users.noreply.github.com>
2024-04-01 14:50:55 -0500

    Update ingresar.js
    protocolo hhtps

--- js/ingresar.js
+++ js/ingresar.js
@@ -61,7 +61,7 @@ document.addEventListener('DOMContentLoaded', function () {
         // Guardar el nuevo valor de turno en el localStorage
         localStorage.setItem('turno', turno);
 
-        fetch('http://sistema-polania-production.up.railway.app/servicios/save', {
+        fetch('https://sistema-polania-production.up.railway.app/servicios/save', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
