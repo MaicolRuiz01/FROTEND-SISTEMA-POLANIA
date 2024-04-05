5a2923327e13536983fcc4a9a2d2c3e85a8dc15a
MiltonPolania <multisrepuestospolania@gmail.com>
2024-04-05 16:06:44 -0500

    correcion de formulario de ingreso

--- js/ingresar.js
+++ js/ingresar.js
@@ -68,29 +68,30 @@ document.addEventListener('DOMContentLoaded', function () {
             },
             body: JSON.stringify(datos)
         })
-        .then(function (response) {
-            // Verificar si la solicitud fue exitosa
-            if (response.ok) {
-                // Si la respuesta es exitosa, mostrar una alerta de éxito
-                mostrarExito('Servicio guardado exitosamente.');
-                
-            } else {
-                // Si hay un error en la respuesta, obtener el mensaje de error del servidor
-                response.json().then(function (data) {
-                    // Mostrar una alerta de error con el mensaje del servidor
-                    mostrarError('Error: ' + data.message);
-                }).catch(function (error) {
-                    // Si no se puede obtener el mensaje de error del servidor, mostrar un mensaje genérico
-                    mostrarError('Error en la respuesta del servidor.');
-                });
-            }
-        })
-        .catch(function (error) {
-            // Capturar y manejar errores de la solicitud
-            console.error('Error:', error);
-            // Mostrar una alerta de error genérico
-            mostrarError('Ocurrió un error al guardar el servicio.');
-        });
+            .then(function (response) {
+                // Verificar si la solicitud fue exitosa
+                if (response.ok) {
+                    // Si la respuesta es exitosa, mostrar una alerta de éxito
+                    mostrarExito('Servicio guardado exitosamente.');
+
+
+                } else {
+                    // Si hay un error en la respuesta, obtener el mensaje de error del servidor
+                    response.json().then(function (data) {
+                        // Mostrar una alerta de error con el mensaje del servidor
+                        mostrarError('Error: ' + data.message);
+                    }).catch(function (error) {
+                        // Si no se puede obtener el mensaje de error del servidor, mostrar un mensaje genérico
+                        mostrarError('Error en la respuesta del servidor.');
+                    });
+                }
+            })
+            .catch(function (error) {
+                // Capturar y manejar errores de la solicitud
+                console.error('Error:', error);
+                // Mostrar una alerta de error genérico
+                mostrarError('Ocurrió un error al guardar el servicio.');
+            });
     });
 });
 
@@ -105,9 +106,8 @@ function mostrarExito(mensaje) {
     }).then((result) => {
         if (result.isConfirmed) {
             // Cerrar el modal
-            $('#formulario').modal('hide');
-            // Actualizar la tabla
             obtenerDatos();
+            window.location.href = 'index.html';
         }
     });
 }
@@ -123,46 +123,46 @@ function mostrarError(mensaje) {
     });
 }
 
-function mostrar_impresoras(){
+function mostrar_impresoras() {
     connetor_plugin.obtenerImpresoras()
-                .then(impresoras => {                    
-                 console.log(impresoras)
-                });
+        .then(impresoras => {
+            console.log(impresoras)
+        });
 }
 
 
-async function imprimir(){
+async function imprimir() {
     let nombreImpresora = "SAM4S ELLIX20II";
     let api_key = "12345"
-    
-   
-    const conector = new connetor_plugin()
-                conector.fontsize("2")
-                conector.textaling("center")
-                conector.text("MULTIREPUESTOS POLANIA")
-                conector.fontsize("1")
-                conector.text("Calle de las margaritas #45854")
-                conector.text("PEC7855452SCC")
-                conector.feed("3")
-                conector.textaling("left")
-                conector.text("Fecha: Miercoles 8 de ABRIL 2022 13:50")                        
-                conector.text("Cant.       Descripcion      Importe")
-                conector.text("------------------------------------------")
-                conector.text("1- Barrote 2x4x8                    $110")
-                conector.text("3- Esquinero Vinil                  $165")
-                conector.feed("1")
-                conector.fontsize("2")
-                conector.textaling("center")
-                conector.text("Total: $275")
-                conector.qr("https://abrazasoft.com")
-                conector.feed("5")
-                conector.cut("0") 
 
-            const resp = await conector.imprimir(nombreImpresora, api_key);
-            if (resp === true) {              
-            
-            } else {
-                 console.log("Problema al imprimir: "+resp)                    
-            
-            }
+
+    const conector = new connetor_plugin()
+    conector.fontsize("2")
+    conector.textaling("center")
+    conector.text("MULTIREPUESTOS POLANIA")
+    conector.fontsize("1")
+    conector.text("Calle de las margaritas #45854")
+    conector.text("PEC7855452SCC")
+    conector.feed("3")
+    conector.textaling("left")
+    conector.text("Fecha: Miercoles 8 de ABRIL 2022 13:50")
+    conector.text("Cant.       Descripcion      Importe")
+    conector.text("------------------------------------------")
+    conector.text("1- Barrote 2x4x8                    $110")
+    conector.text("3- Esquinero Vinil                  $165")
+    conector.feed("1")
+    conector.fontsize("2")
+    conector.textaling("center")
+    conector.text("Total: $275")
+    conector.qr("https://abrazasoft.com")
+    conector.feed("5")
+    conector.cut("0")
+
+    const resp = await conector.imprimir(nombreImpresora, api_key);
+    if (resp === true) {
+
+    } else {
+        console.log("Problema al imprimir: " + resp)
+
+    }
 }
