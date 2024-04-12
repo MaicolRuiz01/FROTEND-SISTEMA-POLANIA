a21070f86e59c0426b8de925754761903d270efc
MiltonPolania <multisrepuestospolania@gmail.com>
2024-04-12 15:01:35 -0500

    corte 2

--- js/imprimir.js
+++ js/imprimir.js
@@ -1,24 +1,24 @@
 const URLPlugin = "http://localhost:8000"
 
-function mostrar_impresoras(){
+function mostrar_impresoras() {
     connetor_plugin.obtenerImpresoras()
-                .then(impresoras => {                    
-                 console.log(impresoras)
-                });
+        .then(impresoras => {
+            console.log(impresoras)
+        });
 }
 
 const imprimirImagenes = async (nombreImpresora) => {
     const conector = new ConectorPluginV3(URLPlugin);
     conector.Iniciar();
-        conector.CorteParcial();
-        conector.EscribirTexto("Multirepuestos Polania");
-        conector.Feed(1);
-        conector.Iniciar(); //Nota: esto solo es necesario en ocasiones, por ejemplo en mi impresora debo hacerlo siempre que acabo de imprimir una imagen
-        conector.Feed(1);
-        conector.Corte(2);
-        conector.CorteParcial();
-    
-   
+    conector.Corte(1);
+    conector.EscribirTexto("Multirepuestos Polania");
+    conector.Feed(1);
+    conector.Iniciar(); //Nota: esto solo es necesario en ocasiones, por ejemplo en mi impresora debo hacerlo siempre que acabo de imprimir una imagen
+    conector.Feed(1);
+    conector.Corte(1);
+
+
+
     const respuesta = await conector
         .imprimirEn(nombreImpresora);
     if (respuesta === true) {
