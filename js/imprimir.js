1519efb86d5c4ee239a1d17da25a8bca29282fb5
MiltonPolania <multisrepuestospolania@gmail.com>
2024-04-12 14:54:50 -0500

    corte impresora

--- js/imprimir.js
+++ js/imprimir.js
@@ -10,7 +10,7 @@ function mostrar_impresoras(){
 const imprimirImagenes = async (nombreImpresora) => {
     const conector = new ConectorPluginV3(URLPlugin);
     conector.Iniciar();
-   
+        conector.CorteParcial();
         conector.EscribirTexto("Multirepuestos Polania");
         conector.Feed(1);
         conector.Iniciar(); //Nota: esto solo es necesario en ocasiones, por ejemplo en mi impresora debo hacerlo siempre que acabo de imprimir una imagen
@@ -22,8 +22,6 @@ const imprimirImagenes = async (nombreImpresora) => {
     const respuesta = await conector
         .imprimirEn(nombreImpresora);
     if (respuesta === true) {
-        conector.Corte(2);
-        conector.CorteParcial();
         alert("Impreso correctamente");
     } else {
         alert("Error: " + respuesta);
